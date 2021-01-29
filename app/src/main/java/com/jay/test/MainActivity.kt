901ae867edd5dbaa1bb667fa8ccd12fffc97c95d
DESKTOP-TMGSLQR\Sokol <sokirko0601@gmail.com>
2021-01-30 01:01:32 +0200

    implementing library

--- app/src/main/java/com/jay/test/MainActivity.kt
+++ app/src/main/java/com/jay/test/MainActivity.kt
@@ -2,11 +2,23 @@ package com.jay.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sokyrko.liberty.Liberty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Liberty.init(this)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Liberty.onRequestPermissionsResult(this, requestCode, permissions, grantResults)
     }
 }
